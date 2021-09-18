a3f8de52fd8a1dbb4739b04b149e41ddaf2584df
abdurazzak2002 <toloev.abi97@gmail.com>
2021-09-18 13:46:11 +0600

    1

---
+++ lesson_5.py
@@ -0,0 +1,57 @@
+# names = ['Ali', 'Baiel', 'klara', 'jonh']
+# empty_names = []
+# for name in names:
+#     empty_names.append(names.pop())
+# print(empty_names)
+# print(names)
+
+# languages = ['python', 'JS', 'Java', 'C#']
+# for language in languages:
+#     print(language.upper()+' is the best PL')
+
+
+# умнежение списка
+# number = [0]
+# numbers = number*100
+# print(len(numbers))
+# print(numbers)
+# num = list(range(1,1000000))
+# print(num)
+
+# numbers = [1,2,3,4,5]
+# total = 1
+# for number in numbers:
+#     total = total * number     ## можно написать total *= number
+# print(total)
+
+# генератор списков range()
+# numbers = list(range(1,50))
+
+
+# square_list = []
+# for number in range(1,21):
+#     square_list.append(number**2)
+# print(square_list)
+
+# numbers = [number for number in range(1,100000)]
+# print(numbers)
+
+# names = ['Ali', 'Baiel', 'klara', 'jonh']
+# upper_names = [name.upper() for name in names]
+# print(upper_names)
+
+
+# копирование списка
+# names = ['Ali', 'Baiel', 'klara', 'jonh']
+# nan = names
+# print(nan)
+# nan.uppend('Akbar')
+# print('nan', nan)
+# print('names', names)
+
+
+# numbers = [1,2,3,4,5]
+# nan = numbers[:]
+# nan.append(10)
+# print(nan)
+# print(numbers)
